526f9bb207815f5f7b445374c3ea314219061b3b
Slupshi <slupshi@gmail.com>
2023-05-26 11:00:07 +0200

    feat: dart run

---
+++ lib/models/hero_model.dart
@@ -0,0 +1,25 @@
+import 'package:e_rose/models/accident_type_model.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'hero_model.freezed.dart';
+part 'hero_model.g.dart';
+
+@freezed
+class HeroModel with _$HeroModel {
+  const factory HeroModel({
+    required int id,
+    required String heroName,
+    required String? firstName,
+    required String? lastName,
+    required String email,
+    required String phoneNumber,
+    required double longitude,
+    required double latitude,
+    int? heroScore,
+    // ignore: invalid_annotation_target
+    @JsonKey(name: 'accidents') required List<AccidentTypeModel>? accidentTypes,
+  }) = _HeroModel;
+
+  factory HeroModel.fromJson(Map<String, Object?> json) =>
+      _$HeroModelFromJson(json);
+}
